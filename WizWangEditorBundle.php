bbbeef39fe491eb881aaa746ed64fb89bc758ae3
jack <linjue@wilead.com>
2017-01-16 14:38:15 +0800

    no message

---
+++ WizWangEditorBundle.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * WizWangEditorBundle.php
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    jack <linjue@wilead.com>
+ * @copyright 2007-2017/1/16 WIZ TECHNOLOGY
+ * @link      https://wizmacau.com
+ * @link      https://lamjack.github.io
+ * @link      https://github.com/lamjack
+ * @version
+ */
+namespace Wiz\WangEditorBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class WizWangEditorBundle
+ * @package Wiz\WangEditorBundle
+ */
+class WizWangEditorBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
